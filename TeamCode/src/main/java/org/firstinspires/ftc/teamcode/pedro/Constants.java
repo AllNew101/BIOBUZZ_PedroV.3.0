8ed0a5388018646de5002541fbca66a43f32f3f9
AllNew101 <kangood23626@gmail.com>
2026-09-10 17:33:35 +0700

    Tune introduction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.algorithm.ForesightConfig;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
+import com.pedropathing.revhub.localizers.PinpointConfig;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+    public static MecanumConfig drivetrainConfig;
+    public static PinpointConfig localizerConfig;
+    public static ForesightConfig foresightConfig;
+
     public static Follower create(HardwareMap h) {
         // return new Follower(Drivetrain, Localizer, Foresight);
         return null;
